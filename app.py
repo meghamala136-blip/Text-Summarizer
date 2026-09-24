a0a5d6d7386b1c97cec20ac8de880ddc5ab7d9b5
meghamala136-blip <meghamala136@gmail.com>
2026-09-24 20:50:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
+import os
 import nltk
+
 try:
     nltk.data.find('tokenizers/punkt')
 except LookupError:
@@ -114,5 +116,6 @@ def download():
     )
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
